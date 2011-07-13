b5853e9b52099ffd7bf55acbac0803b06c578a1d
Makis Tracend <makis.tracend@gmail.com>
2011-07-13 12:38:03 +1000

    Normalising paths to new constants convention

--- html/index.php
+++ html/index.php
@@ -11,23 +11,27 @@ Licensed under the GNU - http://www.gnu.org/licenses/gpl-2.0.txt
 
 // main constants that define the core/clone model - include a BASE constant to where the core files live
 define('APP', realpath("../").'/app/'); //with trailing slash pls
+// the location of the website in relation with the domain root
 
-// full path of where the templates reside
-define('TEMPLATES', $_SERVER['DOCUMENT_ROOT'] . '/templates/'); 
-// the url of your cdn, if you're using one
-define('ASSETS', 'http://' . $_SERVER['SERVER_NAME'] . '/assets/'); 
+define('WEB_FOLDER','/kisscms/html/');
+// alternatively use this if you do not have mod_rewrite enabled
+//define('WEB_FOLDER','/index.php/'); 
+
 // the location of the SQLite database
 define('DATA', realpath("../").'/data/');
 
-// the location of the website in relation with the domain root
-define('WEB_FOLDER','/');
-// alternatively use this if you do not have mod_rewrite enabled
-//define('WEB_FOLDER','/index.php/'); 
+// full path of where the templates reside
+define('TEMPLATES', $_SERVER['DOCUMENT_ROOT'] . WEB_FOLDER . '/templates/'); 
+
+// the url of your cdn, if you're using one
+define('ASSETS', 'http://' . $_SERVER['SERVER_NAME'] . '/assets/'); 
+
 
 //===============================================
 // Global Variables
 //===============================================
 $GLOBALS['sitename']='KISSCMS - Lightweight CMS based on the KISSMVC Framework';
+
 
 //===============================================
 // Start the controller

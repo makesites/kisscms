b16d95884ef220ff1a083cce9b1d702a037fca07
Makis Tracend <makis.tracend@gmail.com>
2011-07-19 18:25:41 +1000

    Introducing congig controller and database

---
+++ app/models/Config.php
@@ -0,0 +1,46 @@
+<?php
+class Config extends Model {
+
+  function __construct() {
+    parent::__construct('config.sqlite','id','main'); //primary key = id; tablename = pages
+    /*$this->rs['id'] = '';
+    $this->rs['title'] = '';
+    $this->rs['content'] = '';
+    $this->rs['path'] = '';
+    $this->rs['date']= '';
+    $this->rs['tags']= '';
+    $this->rs['template']= '';
+    if ($id)
+      $this->retrieve($id);*/
+  }
+
+  function create() {
+    $this->rs['date']=date('Y-m-d H:i:s');
+    return parent::create();
+  }
+
+  function update() {
+    $this->rs['date']=date('Y-m-d H:i:s');
+    return parent::update();
+  }
+
+  function get_tables(){
+	// SELECT name FROM sqlite_master WHERE type = "table";  
+  }
+
+  function get_page_from_path( $uri ) {
+    $dbh= $this->getdbh( DB_PAGES );
+    $sql = 'SELECT * FROM "pages" WHERE "path"="'. $uri . '" LIMIT 1';
+    $results = $dbh->prepare($sql);
+    //$results->bindValue(1,$username);
+    $results->execute();
+    $page = $results->fetch(PDO::FETCH_ASSOC);
+    if (!$page)
+      return false;
+    foreach ($page as $k => $v)
+      $this->set($k,$v);
+    return true;
+  }
+
+}
+?>

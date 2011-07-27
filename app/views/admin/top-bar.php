5816dcf9bac0ced34eb4ec0b3bc416ae2154e6af
Makis Tracend <makis.tracend@gmail.com>
2011-07-27 13:44:06 +1000

    Functioning delete action

--- app/views/admin/top-bar.php
+++ app/views/admin/top-bar.php
@@ -7,10 +7,9 @@
 		<li><a href="<?=myUrl("admin/create/".$path, true)?>">Create page</a></li>
 <?php } elseif( isset( $status ) && $status == "edit") { ?>
 		<li><a href="<?=myUrl( $path , true)?>">View page</a></li>
-<?php }  else { ?>
+<?php } ?>
 <?php 	if( isset( $id ) && $id != "1"){ ?>
 		<li><a href="<?=myUrl("admin/delete/$id", true)?>" onclick="return confirm('<?=$GLOBALS['language']['delete_confirm']?>')">Delete page</a></li>
-<?php 	} ?>
 <?php } ?>
 		<li><a href="<?=myUrl("admin/config", true)?>">Configuration</a></li>
 		<li><a href="<?=myUrl("admin/logout", true)?>">Logout</a></li>

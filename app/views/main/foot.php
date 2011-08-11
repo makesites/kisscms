3cffa36979b5cf8c7568b79a787fe283474e130b
Makis Tracend <makis.tracend@gmail.com>
2011-08-11 20:32:05 +1000

    Added js to open external links in new windows by default

--- app/views/main/foot.php
+++ app/views/main/foot.php
@@ -1,21 +1,20 @@
 
 <script type="text/javascript">
-//Configure RequireJS
-require({
-    //Load jQuery before any other scripts, since jQuery plugins normally
-    //assume jQuery is already loaded in the page.
-    priority: ['js/libs/jquery-1.5.1.min']
-});
+//Load jQuery before any other scripts, since jQuery plugins normally
+//assume jQuery is already loaded in the page.
+require(['<?=myUrl()?>/js/libs/jquery-1.5.1.min.js'], function() {
+		
+		// open external links in new window 
+		$('a[rel*=external]').click( function() { 
+			window.open(this.href, '_blank'); return false; 
+		});
 
-// open external links in new window 
-$('a[rel*=external]').click( function() { 
-	window.open(this.href, '_blank'); return false; 
 });
 
 </script>
 
 <!--[if lt IE 7 ]>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
-<script src="js/libs/dd_belatedpng.js"></script>
+<script src="<?=myUrl()?>/js/libs/dd_belatedpng.js"></script>
 <script>DD_belatedPNG.fix("img, .png_bg");</script>
 <![endif]-->
--- app/views/sections/tagcloud.php
+++ app/views/sections/tagcloud.php
@@ -10,7 +10,7 @@
 </div>
 </canvas>
 <script type="text/javascript">
-require(['js/libs/jquery-1.5.1.min', 'js/plugins/jquery.tagcanvas.min'], function() {
+require(['<?=myUrl()?>/js/libs/jquery-1.5.1.min', '<?=myUrl()?>/js/plugins/jquery.tagcanvas.min'], function() {
 		$("#<?=$vars['id']?>").tagcanvas({
 			 depth : 0.75
 		}, "tag-holder");

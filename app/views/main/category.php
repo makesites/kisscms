88ab19b2089787f88b55036fde032912d0c2da52
Makis Tracend <makis.tracend@gmail.com>
2011-08-12 14:49:40 +1000

    Removing html tags from listing summaries

--- app/views/main/category.php
+++ app/views/main/category.php
@@ -2,5 +2,5 @@
 <h2><a href="<?=myUrl($path)?>"><?=$title?></a></h2>
 
 <div>
-<?=truncate($content, 100, " ", "...")?>
+<?=truncate(strip_tags($content), 100, " ", "...")?>
 </div>
--- app/views/sections/tagcloud.php
+++ app/views/sections/tagcloud.php
@@ -10,7 +10,7 @@
 </div>
 </canvas>
 <script type="text/javascript">
-require(['<?=myUrl()?>/js/libs/jquery-1.5.1.min', '<?=myUrl()?>/js/plugins/jquery.tagcanvas.min'], function() {
+require(['<?=myUrl()?>/js/libs/jquery-1.5.1.min.js', '<?=myUrl()?>/js/plugins/jquery.tagcanvas.min.js'], function() {
 		$("#<?=$vars['id']?>").tagcanvas({
 			 depth : 0.75
 		}, "tag-holder");

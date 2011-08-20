a82d7d932f26760df462dd6fdef9af9ff5ef056c
Makis Tracend <makis.tracend@gmail.com>
2011-08-20 18:59:47 +1000

    Changing order of paths

--- html/index.php
+++ html/index.php
@@ -12,36 +12,46 @@ Licensed under the GNU - http://www.gnu.org/licenses/gpl-2.0.txt
 // where the app is located
 define('APP', realpath("../").'/app/'); //with trailing slash pls
 
-// Optional: include a BASE constant here if this is a clone site
-// define('BASE', APP);
+// the location where the SQLite databases will be saved
+define('DATA', realpath("../").'/data/');
 
 // the location of the website in relation with the domain root
 define('WEB_FOLDER','/');
 // alternatively use this if you do not have mod_rewrite enabled
-// define('WEB_FOLDER','/index.php/'); 
-
-// the location where the SQLite databases will be saved
-define('DATA', realpath("../").'/data/');
+//define('WEB_FOLDER','/index.php/'); 
 
 // full path of where the templates reside
 define('TEMPLATES', $_SERVER['DOCUMENT_ROOT'] . WEB_FOLDER . 'templates/'); 
 
-// Optional: you can set the location of your plugins - by default a subdir in the app folder
-// define('PLUGINS', APP . 'plugins/');
 
+// Optional Attributes
+
+// Optional: you can set the location of your plugins - by default a subdir in the app folder
+//define('PLUGINS', APP . 'plugins/');
+
+// find if this is running from localhost
+//define("IS_LOCALHOST", (strpos($_SERVER['SERVER_NAME'], "localhost") !== false) );
+
+//if(IS_LOCALHOST){
+// include a BASE constant here if this is a clone site
+//	define('BASE', APP);
+//} else {
+// include a BASE constant here if this is a clone site
+//	define('BASE', APP);
 // the url of your cdn, if you're using one
-define('CDN', 'http://' . $_SERVER['SERVER_NAME'] . '/'); 
+//	define('CDN', 'http://' . $_SERVER['SERVER_NAME'] . '/'); 
+//}
 
 
 //===============================================
 // Start the controller
 //===============================================
-if (defined("BASE")){ 
+if (defined("APP")){ 
 	// find the clone file first
-	require(BASE.'bin/init.php');
-} elseif (defined("APP")) {
-	// find the core file second
 	require(APP.'bin/init.php');
+} elseif (defined("BASE")) {
+	// find the core file second
+	require(BASE.'bin/init.php');
 } else {
 	quit("Please define the app path in your index file");
 }

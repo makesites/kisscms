09307d347c50d7dbd2d15190d954c4e28e4c9607
Makis Tracend <makis.tracend@gmail.com>
2011-12-13 18:30:30 -0800

    Checking if  is set before looping

--- app/views/sections/ul.php
+++ app/views/sections/ul.php
@@ -3,7 +3,7 @@
 <h3 id="<?=$vars['h3-id']?>" class="<?=$vars['h3-class']?>"><?=$vars['h3']?></h3>
 <? } ?>
 <ul id="<?=$vars['ul-id']?>" class="<?=$vars['ul-class']?>">
-<? if(!empty($items)){ ?>
+<? if( isset($items) && !empty($items) ){ ?>
 <? foreach($items as $item){ ?>
 	<li><a href="<?=$item['url']?>"><?=$item['title']?></a></li>
 <? } 

e35e53e2a194e199e8829504e6aa3218204f91d0
Makis Tracend <makis.tracend@gmail.com>
2011-12-17 20:36:41 -0800

    Updating paths with url() function

--- app/views/main/head.php
+++ app/views/main/head.php
@@ -1,6 +1,6 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 <meta name="title" content="<? Meta::title(); ?>" />
 <meta name="description" content="<? Meta::description(); ?>" />
@@ -8,17 +8,17 @@
 <meta property="og:title" content="<? Meta::title(); ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="<? Meta::url(); ?>" />
-<meta property="og:image" content="/apple-touch-icon-114x114-precomposed.png" />
+<meta property="og:image" content="<?=url('/apple-touch-icon-114x114-precomposed.png')?>" />
 <meta property="og:site_name" content="<?=$config['main']['site_name']?>" />
 <meta property="og:description" content="<? Meta::description(); ?>" />
 
-<base href="<?=str_replace('/index.php','',myUrl('',true))?>" />
+<base href="<?=url('/')?>" />
 
-<link rel="shortcut icon" href="/favicon.ico">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="shortcut icon" href="<?=url('/favicon.ico')?>">
+<link rel="apple-touch-icon" href="<?=url('/apple-touch-icon.png')?>">
 
-<link rel="stylesheet" href="<?=myUrl()?>/css/style.css">
-<link type="text/plain" rel="author" href="<?=myUrl()?>/humans.txt" />
+<link rel="stylesheet" href="<?=url('/css/style.css')?>">
+<link type="text/plain" rel="author" href="<?=url('/humans.txt')?>" />
 
-<script type="text/javascript" src="<?=myUrl()?>/js/libs/require.js"></script>
-<script type="text/javascript" src="<?=myUrl()?>/js/libs/modernizr-1.7.min.js"></script>
+<script type="text/javascript" src="<?=url('/js/libs/require.js')?>"></script>
+<script type="text/javascript" src="<?=url('/js/libs/modernizr-1.7.min.js')?>"></script>

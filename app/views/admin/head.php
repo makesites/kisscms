bcc7d7c81a3d9dbddd8919d9699db6136b26c8a7
Makis Tracend <makis.tracend@gmail.com>
2011-12-27 23:30:44 -0800

    Improving condition to embed the admin assets

--- app/views/admin/head.php
+++ app/views/admin/head.php
@@ -1,4 +1,4 @@
-<?php if(isset($_SESSION['admin'])){ ?>
-	<link href="<?=myCDN('')?>/css/admin.css" rel="stylesheet" type="text/css" media="screen" />
-	<link href="<?=myCDN('')?>/css/jquery.ui.autocomplete.custom.css" rel="stylesheet" type="text/css"  />
+<?php if( (defined("CONTROLLER") && CONTROLLER=="admin") || (array_key_exists('admin', $_SESSION) && $_SESSION['admin'] ) ){ ?>
+	<link href="<?=url('/css/admin.css')?>" rel="stylesheet" type="text/css" media="screen" />
+	<link href="<?=url('/css/jquery.ui.autocomplete.custom.css')?>" rel="stylesheet" type="text/css"  />
 <?php } ?>

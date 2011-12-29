f85d2927bd323b2be00a35d3158e84bcaef2a1b2
Makis Tracend <makis.tracend@gmail.com>
2011-12-28 19:35:43 -0800

    Simplifying conditions for displaying admin options

--- app/views/admin/top-bar.php
+++ app/views/admin/top-bar.php
@@ -1,17 +1,19 @@
 <div id="admin-topbar">
 	<h3><a href="http://kisscms.com/" title="Made with love...">KISSCMS</a></h3>
 	<ul>
-<?php if( isset( $status ) && ($status == "view-page" || $status == "view-category" ) ){ ?>
-		<li><a href="<?=myUrl("admin/edit/$id", true)?>">Edit page</a></li>
-<?php } elseif(  isset( $status ) && $status == "new" ) { ?>
-		<li><a href="<?=myUrl("admin/create/".$path, true)?>">Create page</a></li>
-<?php } elseif( isset( $status ) && $status == "edit") { ?>
-		<li><a href="<?=myUrl( $path , true)?>">View page</a></li>
+<?php if( isset( $id ) ){ ?>
+		<li><a href="<?=url("admin/edit/$id")?>">Edit page</a></li>
+<?php } elseif( isset( $path ) && isset( $status ) && $status == "new" ) { ?>
+		<li><a href="<?=url("admin/create/".$path)?>">Create page</a></li>
+<?php } elseif( isset( $path ) && isset( $status ) && $status == "edit") { ?>
+		<li><a href="<?=url( $path )?>">View page</a></li>
 <?php } ?>
-<?php 	if( isset( $status ) && $status == "view-page" && isset( $id ) && $id != "1"){ ?>
-		<li><a href="<?=myUrl("admin/delete/$id", true)?>" onclick="return confirm('<?=$GLOBALS['language']['delete_confirm']?>')">Delete page</a></li>
+<?php 	if( isset( $id ) && $id != "1"){ ?>
+		<li><a href="<?=url("admin/delete/$id")?>" onclick="return confirm('<?=$GLOBALS['language']['delete_confirm']?>')">Delete page</a></li>
 <?php } ?>
-		<li><a href="<?=myUrl("admin/config", true)?>">Configuration</a></li>
-		<li><a href="<?=myUrl("admin/logout", true)?>">Logout</a></li>
+<?php 	if( !isset( $status ) || (isset( $status ) && $status != "config") ){ ?>
+		<li><a href="<?=url("admin/config")?>">Configuration</a></li>
+<?php } ?>
+		<li><a href="<?=url("admin/logout")?>">Logout</a></li>
 	</ul>
 </div>

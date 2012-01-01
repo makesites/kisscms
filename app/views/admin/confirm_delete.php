d160eb92c4f8ad640341d32a9a84c1cbe1615e19
Makis Tracend <makis.tracend@gmail.com>
2011-12-31 17:43:38 -0800

    Copy updates

--- app/views/admin/confirm_delete.php
+++ app/views/admin/confirm_delete.php
@@ -1,10 +1,10 @@
-<h2>Are you sure?</h2>
+<h2>Deletion Confirmation</h2>
 
 <p>You are about to delete the page: <?=$path?></>
 
 <p>Do you want to proceed?</p>
 
-<form action="<?=myUrl("admin/create")?>">
+<form action="<?=myUrl("admin/delete")?>">
 <input type="hidden" name="path" value="<?=$path?>">
 <input type="submit" value="Delete" />
 </form>
--- app/views/admin/confirm_new.php
+++ app/views/admin/confirm_new.php
@@ -2,7 +2,7 @@
 
 <p>The page you requested does not exist.</>
 
-<p>Would you like to create it now</p>
+<p>Would you like to create it now?</p>
 
 <form action="<?=myUrl("admin/create")?>" method="POST">
 <input type="hidden" name="path" value="<?=$path?>">

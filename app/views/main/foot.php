a01b582e8fbbd2875308d1158f0bb68988dffeda
Makis Tracend <makis.tracend@gmail.com>
2012-05-16 16:02:39 -0700

    Adding global DEBUG flag for JavaScript

--- app/views/main/foot.php
+++ app/views/main/foot.php
@@ -3,3 +3,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
 <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 <![endif]-->
+
+<? if( DEBUG){ ?>
+<script type="text/javascript">var DEBUG = true;</script>
+<? } ?>

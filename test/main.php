ee72d7b4a0a9bf6e178986880b95ec3b7f0a11dc
Makis Tracend <makis.tracend@gmail.com>
2012-05-31 18:05:34 -0700

    Adding Travis CI config

---
+++ test/main.php
@@ -0,0 +1,26 @@
+<?php
+
+class MainTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function setUp()
+    {
+       
+    }
+
+    public function tearDown()
+    {
+        
+    }
+
+    public function testMain()
+    {
+       
+    }
+
+}
+

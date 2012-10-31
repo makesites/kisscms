2a97a4f2022be6724826f9617d1fba99d8b5cd04
Makis Tracend <makis.tracend@gmail.com>
2012-10-30 17:41:12 -0700

    #63 Adding generator meta tag

--- app/views/main/head.php
+++ app/views/main/head.php
@@ -1,25 +1,26 @@
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<meta name="title" content="<? Meta::title(); ?>" />
-<meta name="description" content="<? Meta::description(); ?>" />
-<meta name="author" content="<?=$config['main']['site_author']?>" />
-<meta property="og:title" content="<? Meta::title(); ?>" />
-<meta property="og:type" content="website" />
-<meta property="og:url" content="<? Meta::url(); ?>" />
-<meta property="og:image" content="<?=url('/apple-touch-icon-114x114-precomposed.png')?>" />
-<meta property="og:site_name" content="<?=$config['main']['site_name']?>" />
-<meta property="og:description" content="<? Meta::description(); ?>" />
-<meta property="og:locale" content="en_US" />
+<meta name="title" content="<? Meta::title(); ?>">
+<meta name="description" content="<? Meta::description(); ?>">
+<meta name="author" content="<?=$config['main']['site_author']?>">
+<meta name="generator" content="KISSCMS">
+<meta property="og:title" content="<? Meta::title(); ?>">
+<meta property="og:type" content="website">
+<meta property="og:url" content="<? Meta::url(); ?>">
+<meta property="og:image" content="<?=url('/apple-touch-icon-114x114-precomposed.png')?>">
+<meta property="og:site_name" content="<?=$config['main']['site_name']?>">
+<meta property="og:description" content="<? Meta::description(); ?>">
+<meta property="og:locale" content="en_US">
 
-<base href="<?=url('/')?>" />
+<base href="<?=url('/')?>">
 
 <link rel="shortcut icon" href="<?=url('/favicon.ico')?>">
 <link rel="apple-touch-icon" href="<?=url('/apple-touch-icon.png')?>">
 
 <link rel="stylesheet" href="<?=url('/css/style.css')?>">
-<link type="text/plain" rel="author" href="<?=url('/humans.txt')?>" />
+<link type="text/plain" rel="author" href="<?=url('/humans.txt')?>">
 
 <script type="text/javascript"  data-type="require">
 	var KISSCMS = {};

a484094cfe4b9efd7a2fe4dd1a9e4776ab4d7dcb
Makis Tracend <makis.tracend@gmail.com>
2013-03-14 02:40:17 -0700

    #74 updating conditions for cors

--- app/helpers/mvc.php
+++ app/helpers/mvc.php
@@ -352,14 +352,14 @@ class Controller extends KISS_Controller {
 	function cors() {
 
 		// Allow from any origin
-		if (isset($_SERVER['HTTP_ORIGIN'])) {
-			header("Access-Control-Allow-Origin: $_SERVER['HTTP_ORIGIN']");
-			header('Access-Control-Allow-Credentials: true');
+		if ( isset($_SERVER['HTTP_ORIGIN']) ) {
+			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+			header("Access-Control-Allow-Credentials: true");
 		} else {
 			header("Access-Control-Allow-Origin: *");
 		}
-			header('Access-Control-Allow-Headers: Content-Type, X-Requested-With, X-PINGOTHER');
-			header('Access-Control-Max-Age: 86400');    // cache for 1 day
+			header("Access-Control-Allow-Headers: Content-Type, X-Requested-With, X-PINGOTHER");
+			header("Access-Control-Max-Age: 86400");    // cache for 1 day
 			header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
 		
 	}

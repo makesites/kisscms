37b884a13ae2a484e4b8cce826f1087185bc97d9
Makis Tracend <makis.tracend@gmail.com>
2013-06-21 05:06:29 -0700

    better outlining of auth behavior

--- app/helpers/auth.php
+++ app/helpers/auth.php
@@ -32,22 +32,20 @@ class KISS_Auth extends Controller {
 		// convention: lookup for a 'user' object in the db array
 		$db= ( !empty($this->db['user']) ) ? $this->db['user'] : false;
 		
-		// just for testing - remove in prod
-		unset( $_SESSION['user'] );
-		
+        // STEP 1: find the user id
 		if( !empty($_SESSION['user']['id']) ){
 			// if there is a session we are ok
 			$id = $_SESSION['user']['id'];
 			// check last accessed 
-			$accessed = strtotime("now") - strtotime( $_SESSION['user']['updated'] );
+			$accessed = ( !empty($_SESSION['user']['updated']) ) ? strtotime("now") - strtotime( $_SESSION['user']['updated'] ) : false;
 			// it's been less than 2 min so just return the session model (to avoid latency from constant db/api requests)
-			if( $accessed < 120 ) return $_SESSION['user'];
+			//if( $accessed && $accessed < 120 ) return $_SESSION['user'];
 			
 		} else if( $api && $api->login() ) {
 			// the service has found a valid login
 			// lookup info from the remote service
 			$me = $api->me();
-			$id = $me['id'];
+            $id = $me['id'];
 			
 		} else if( !empty( $_COOKIE['user'] ) ) {
 			// empty user session AND oauth token
@@ -55,28 +53,35 @@ class KISS_Auth extends Controller {
 			$id = $_COOKIE['user'];
 		}
 		
-		// no id = assume the user is not logged in
+		// if no id -> assume the user is not logged in
 		if( !isset( $id ) ) return false;
 		
-		// READ
+		// STEP 2: Read the user model
 		// - check if there's a user in the DB
 		// following logic needs to be conditioned in case the $db is not setup
 		$store = $db->read( $id );
 		
-		// UPDATE
-		// - update with the remote serive (if called)
+        // STEP 3: Update credentials
+        // - update existing data with the remote service
 		if( isset( $me ) ) { 
 			$db->merge( $me );
-			// - add the latest oauth info
-			$db->set('oauth', $api->oauth->creds() );
-		} else {
+		}
+        // - if logged in to the api assume it has the latest oauth info
+        if( $api && $api->login() ) { 
+            //
+            $db->set('oauth', $api->oauth->creds() );
+            
+		} else if( !is_null( $db->get('oauth') ) ) {
+            // check for credentials that have expired?
 			// use the creds from the db
 			$api->oauth->creds( $db->get('oauth') );
-			// re-initiate service
+			// re-initiate service (to login to the api)
 			$api->init();
 		}
+		// - record the time
+		$db->set("updated", date('Y-m-d H:i:s') );
 		
-		// SAVE
+        // STEP 4: SAVE back to the db
 		// - update the user model
 		$method = ( !$store) ? "create" : "update";
 		$db->{$method}();

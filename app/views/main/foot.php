5112ac45b756b4c516b9fcf9b99a7070ca03c118
Makis Tracend <makis.tracend@gmail.com>
2013-06-26 05:52:27 -0700

    #88 removing fixed require.js callback to init()

--- app/views/main/foot.php
+++ app/views/main/foot.php
@@ -4,8 +4,4 @@
 <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 <![endif]-->
 
-<script type="text/javascript">
-	//Object.extend(KISSCMS, {"require":{"callback": function(){ init();} }});
-</script>
-
 <script id="require-main" type="text/javascript" data-main="<?=url("/client")?>" src="<?=url("/js/libs/require.js")?>"  defer="defer"></script>

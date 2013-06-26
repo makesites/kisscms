1746bc6c39b1fdbc8028d3753e1e67c84e67751a
Makis Tracend <makis.tracend@gmail.com>
2013-06-26 02:24:25 -0700

    #39 fixing loading dependencies for admin tags

--- app/views/admin/edit_page.php
+++ app/views/admin/edit_page.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 // create a fallback for the variables we are using
-$title = (!isset($title)) ? '' : $title; 
-$content = (!isset($content)) ? '' : $content; 
-$action = ( $status == "create" ) ? url("admin/update", true) : url("admin/update/id/$id", true); 
+$title = (!isset($title)) ? '' : $title;
+$content = (!isset($content)) ? '' : $content;
+$action = ( $status == "create" ) ? url("admin/update", true) : url("admin/update/id/$id", true);
 ?>
 
 <h2><?=$GLOBALS['language'][$status.'_title']?></h2>
@@ -17,30 +17,29 @@ $action = ( $status == "create" ) ? url("admin/update", true) : url("admin/updat
 	<textarea id="content" name="content"><?=$content?></textarea>
 
 	<label for="tags">Tags</label>
-    <p>(Separate tags with commas)</p>
+	<p>(Separate tags with commas)</p>
 	<input type="text" name="tags" id="tags" value="<?=$tags?>" />
-	
-    <label for="template">Template</label>
-    <?=Template::doList($template);?>
-    
-    <label for="content">Page URL</label>
+
+	<label for="template">Template</label>
+	<?=Template::doList($template);?>
+
+	<label for="content">Page URL</label>
 	<p><?=url("$path")?></p>
 	<input type="hidden" name="path" value="<?=$path?>" />
 
-	
+
 	<input type="submit" value="<?=$GLOBALS['language'][$status.'_button']?>" id="edit-button" class="button" />
 </form>
 
+<script type="text/javascript" src="<?=url('/js/libs/jquery-ui-1.8.core-and-interactions.min.js')?>" data-type="minify/require" data-group="admin" data-deps="jquery"></script>
+<script type="text/javascript" src="<?=url('/js/libs/jquery-ui-1.8.autocomplete.min.js')?>" data-type="minify/require" data-group="admin" data-deps="jquery"></script>
+<script type="text/javascript" src="<?=url('/js/plugins/tag-it.js')?>" data-type="minify/require" data-group="admin" data-deps="jquery"></script>
 <script type="text/javascript">
-require(['/js/libs/jquery-1.7.1.min.js'], function() {
-	
-	require(['<?=url('/js/libs/jquery-ui-1.8.core-and-interactions.min.js')?>', '<?=url('/js/libs/jquery-ui-1.8.autocomplete.min.js')?>', '<?=url('/js/plugins/tag-it.js')?>'], function() {
-			$(document).ready(function(){
-				$("#tags").tagit({
-					availableTags: []
-				});
-			});
-	});
 
+$(function(){
+	$(".cms-form #tags").tagit({
+		availableTags: []
+	});
 });
+
 </script>

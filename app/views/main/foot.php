33d12affa3bd6503266426d0ef5690bddd70d131
Makis Tracend <makis.tracend@gmail.com>
2013-06-26 17:13:50 -0700

    #88 restoring init() js, if available

--- app/views/main/foot.php
+++ app/views/main/foot.php
@@ -5,8 +5,7 @@
 <![endif]-->
 
 <script type="text/javascript" data-type="client">
-	Object.extend(KISSCMS, <?=json_encode_escaped( $GLOBALS['client'] )?>);
-	require.config( KISSCMS.require );
+	Object.extend(KISSCMS, {"require":{"callback": function(){ if(typeof init != "undefined") init(); } }});
 </script>
 
 <script id="require-main" type="text/javascript" data-main="<?=url("/client")?>" src="<?=url("/js/libs/require.js")?>"  defer="defer"></script>

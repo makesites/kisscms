a1c6baeb510b42af10a3cdcabcc05c6d329af73d
Makis Tracend <makis.tracend@gmail.com>
2013-06-30 03:40:03 -0700

    #100 using an id column to delete duplicate keys

--- app/models/Config.php
+++ app/models/Config.php
@@ -2,37 +2,40 @@
 class Config extends Model {
 
 	function __construct($id=0, $table='sqlite_master') {
-	$this->id = $id;
-	$this->pkname = 'key';
-	$this->tablename = $table;
-	parent::__construct('config.sqlite', $this->pkname, $this->tablename); //primary key = id; tablename = sqlite_master
-	$this->rs['key'] = '';
+		$this->id = $id;
+		$this->pkname = 'id';
+		$this->tablename = $table;
+		parent::__construct('config.sqlite', $this->pkname, $this->tablename); //primary key = id; tablename = sqlite_master
+		$this->rs['id'] = $id;
+		$this->rs['key'] = '';
 		$this->rs['value'] = '';
+		if ($id) $this->retrieve($id);
 	}
 
 	static function register($table, $key, $value="") {
-	// stop if variable already available
-	if( !empty($GLOBALS['config'][$table][$key]) ) return false;
+		// exit now if variable already available
+		$key_exists = !( empty($GLOBALS['config'][$table][$key]) && is_null($GLOBALS['config'][$table][$key]) );
+		if ( $key_exists ) return;
 
 	// then check if the table exists
 	if( empty($GLOBALS['config'][$table]) ){
 		$config = new Config(0, $table);
-		$config->create_table($table, implode(",", array_keys( $config->rs )) );
+		// FIX: The id needs to be setup as autoincrement
+		$config->create_table($table, "id INTEGER PRIMARY KEY ASC," . implode(",", array_keys( $config->rs )) );
 		$GLOBALS['config'][$table] = array();
 	}
 
-	// just create the key
-	if( empty($GLOBALS['config'][$table][$key]) ) {
+		// we already know the key doesn't exist - just create it
 		$config = new Config(0, $table);
 		$config->set('key', "$key");
 		$config->set('value', "$value");
 		$config->create();
 		// save in the global object
 		$GLOBALS['config'][$table][$key] = $value;
-	}
 
 	}
 
+	// loading config - removing duplicate entries
 	function getConfig(){
 		$config = array();
 		// get the raw db output
@@ -42,7 +45,17 @@ class Config extends Model {
 		// clean up data in a better format
 		foreach( $table_rows as $table => $rows ){
 			foreach( $rows as $row ){
-				$config[$table][$row['key']] = $row['value'];
+				// delete a duplicate key
+				if( array_key_exists( $row['key'], $config[$table] ) ){
+					// backwards compatibility - see if there's an id available
+					if( $row['id'] ){
+						$c = new Config($row['id'], $table);
+						// delete entry
+						$c->delete();
+					}
+				} else {
+					$config[$table][$row['key']] = $row['value'];
+				}
 			}
 		}
 		// verify config against the setup

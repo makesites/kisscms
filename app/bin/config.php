e7c2086521b1597943f5ec2b8f8baf6bb952a9a2
Makis Tracend <makis.tracend@gmail.com>
2013-06-29 23:52:23 -0700

    #99 adding config option for uglify service

--- app/bin/config.php
+++ app/bin/config.php
@@ -2,38 +2,40 @@
 
 
 //===============================================
-// KISSCMS Settings 
+// KISSCMS Settings
 //===============================================
 
-if( class_exists('Config') && method_exists(new Config(),'register')){ 
+if( class_exists('Config') && method_exists(new Config(),'register')){
 	$config = new Config();
 	$GLOBALS['config'] = $config->getConfig();
-	
+
 	// Register variables
 	Config::register("main", "site_name", "KISSCMS");
 	Config::register("main", "site_description", "Lightweight CMS based on the KISSMVC Framework");
 	Config::register("main", "site_author", "Your Name or Company");
-	
+
 	Config::register("main", "default_route", "main");
 	Config::register("main", "default_action", "index");
-	
+
 	Config::register("main", "default_template", "default.php");
-	
+
 	Config::register("main", "db_pages", "pages.sqlite");
-	
+
+	Config::register("compress", "uglify_service", "http://uglify.makesit.es/");
+
 	Config::register("admin", "admin_username", "admin");
 	Config::register("admin", "admin_password", '$1$VGthOaJJ$o7GR6b657c7mtrOCTLZgO.');
-	
+
 	// Definitions
 	define('DEFAULT_ROUTE', $GLOBALS['config']['main']['default_route']);
 	define('DEFAULT_ACTION', $GLOBALS['config']['main']['default_action']);
-	
+
 	define('DEFAULT_TEMPLATE', $GLOBALS['config']['main']['default_template']);
 	define("ADMIN_TEMPLATE", "admin.php");
 	define("LISTINGS_TEMPLATE", "listings.php");
-	
-	
-	define('DB_PAGES', $GLOBALS['config']['main']['db_pages']); 
+
+
+	define('DB_PAGES', $GLOBALS['config']['main']['db_pages']);
 }
 
 //===============================================

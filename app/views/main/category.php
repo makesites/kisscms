51a54f6d4f040c8ea1ae877b9d7da1242ce2e5ce
Makis Tracend <makis.tracend@gmail.com>
2013-06-30 20:28:57 -0700

    #101 not displaying category summary if not available

--- app/views/main/category.php
+++ app/views/main/category.php
@@ -1,6 +1,9 @@
 <hr />
 <h2><a href="<?=url($path)?>"><?=$title?></a></h2>
 
+<? $_summary = ""; $_summary = truncate(strip_tags($content), 100, " ", "..."); ?>
+<? if( !empty($_summary) ){ ?>
 <div>
-<?=truncate(strip_tags($content), 100, " ", "...")?>
+<?=$_summary ?>
 </div>
+<? } ?>

e6c97dd9743ab56337fc806f20a45ec86b6831d0
Makis Tracend <makis.tracend@gmail.com>
2013-11-30 23:38:45 -0800

    #95 removing boilerplate comments from default template

--- public/templates/default.php
+++ public/templates/default.php
@@ -1,8 +1,5 @@
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html>
 <head>
 	<title><? Meta::title() ?></title>
 	<? Template::head(); ?>
@@ -44,7 +41,6 @@
 </div>
 
 <? Template::foot(); ?>
-
 
 </body>
 </html>

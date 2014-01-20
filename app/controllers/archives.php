8511dcd07a73ed70b904252004071346a50cb79d
Makis Tracend <makis.tracend@gmail.com>
2014-01-19 16:26:50 -0800

    declaring render() with same params in children controllers

--- app/controllers/archives.php
+++ app/controllers/archives.php
@@ -4,47 +4,47 @@ class Archives extends Controller {
 
 	public $data;
 	public $date;
-	
+
 	//This function maps the controller name and function name to the file location of the .php file to include
 	function index( $params ) {
-		
+
 		// data is being passed as a key-value pair
-		foreach( $params as $k => $v ){ 
+		foreach( $params as $k => $v ){
 			$this->data['date'] = $k ."-". $v;
 			// only loop throught he first item
 			break;
 		}
-		
+
 		// load the index
 		$this->render();
 	}
 
-	function render() {
-		
+	function render( $view=false ) {
+
 		// get the page details stored in the database
 		$this->requestAllPages();
-		
+
 		// define the rendering template
 		if( !$this->data['template'] ) $this->data['template'] = LISTINGS_TEMPLATE;
-		
+
 		// display the page
 		Template::output($this->data);
 	}
-	
+
 	function requestAllPages() {
 
 		$page=new Page();
 		$page->tablename = "pages";
 		$pages = $page->retrieve_many("date LIKE '%".$this->data['date']."%'");
 		$view = getPath('views/archives/body.php');
-		
+
 		foreach( $pages as $data ){
 			$data['view'] = $view;
 			$this->data['body'][] = $data;
 		}
 
 	}
-	
+
 
 }
 
--- app/controllers/tag.php
+++ app/controllers/tag.php
@@ -4,65 +4,65 @@ class Tag extends Controller {
 
 	public $data;
 	public $tag;
-	
+
 	//This function maps the controller name and function name to the file location of the .php file to include
 	function index( $params ) {
-		
+
 		// the main controler is a special case that has only one parameter - the url
 		$this->data['path'] = preg_replace('#^'.addslashes(WEB_FOLDER).'#', '', $_SERVER['REQUEST_URI']);
-		// check if we have a trailing slash (and remove it) 
+		// check if we have a trailing slash (and remove it)
 		$this->data['path'] = ( substr($this->data['path'], -1) == "/" ) ? substr($this->data['path'], 0, -1) : $this->data['path'];
-		
+
 		$this->tag = $params;
-		
+
 		// load the index
 		$this->render();
 	}
 
-	function render() {
-		
+	function render( $view=false ) {
+
 		// get the page details stored in the database
 		$this->requestAllPages();
-		
+
 		// define the rendereing template
 		if( !$this->data['template'] ) $this->data['template'] = LISTINGS_TEMPLATE;
-		
+
 		// display the page
 		Template::output($this->data);
 	}
-	
+
 	function requestAllPages() {
 
 		$page=new Page();
 		$page->tablename = "pages";
 		$pages = $page->retrieve_many("tags like '%".$this->tag."%'");
 		$view = getPath('views/tag/body.php');
-		
+
 		foreach( $pages as $data ){
 			$data['view'] = $view;
 			$this->data['body'][] = $data;
 		}
 
 	}
-	
+
 	public static function getBody( $path ) {
 		// pls replace strng $path with proper params array (make parameter generation more abstruct)
 		$items = array();
 		$tag = preg_replace('#^tag/#', '', $path);
-		
+
 		$page=new Page();
 		$page->tablename = "pages";
 		$pages = $page->retrieve_many("tags like '%".$tag."%'");
 		$view = getPath('views/tag/body.php');
-		
+
 		foreach( $pages as $data ){
 			$data['view'] = $view;
 			$items[] = $data;
 		}
-		
+
 		return $items;
 	}
-	
+
 }
 
 

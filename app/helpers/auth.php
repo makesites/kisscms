93bd9d8094dac1ebf1201d6e7a919e2dd3aa61f8
Makis Tracend <makis.tracend@gmail.com>
2014-02-06 04:57:20 -0800

    #125 using independent id for user (from any api)

--- app/helpers/auth.php
+++ app/helpers/auth.php
@@ -49,49 +49,66 @@ class KISS_Auth extends Controller {
 			// the service has found a valid login
 			// lookup info from the remote service
 			$me = $api->me();
-			$id = $me['id'];
+			try {
+				$result = $db->findOne("accounts", array($api->name, $me['id']) );
+				if( !empty( $result ) ) {
+					$id = $db->get("id");
+					$synced = true; // this should become a flag of the base Model class
+				}
+			} catch( Exception $e ){
+				// legacy api (remove soon)
+				$id = $me['id'];
+			}
 
 		} else if( !empty( $_COOKIE['user'] ) ) {
 			// empty user session AND oauth token
 			// lookup the client cookie (as a last resort)
 			$id = $_COOKIE['user'];
 		}
-
+		// Final exit for non members...
 		// if no id -> assume the user is not logged in
-		if( !isset( $id ) ) return false;
+		// if $me['id'] exists it means an api connection has been made..
+		if( !isset( $id ) && empty($me['id']) ) return false;
 
 		// STEP 2: Read the user model
 		// - check if there's a user in the DB
 		// following logic needs to be conditioned in case the $db is not setup
-		$store = $db->read( $id );
+		if( isset( $id ) && !isset($synced) ) $db->read( $id );
 
 		// STEP 3: Update credentials
-		// - update existing data with the remote service
+		// - update existing data with the remote service (not sure about this one...)
 		if( isset( $me ) ) {
+			// filter fields (use clone array?)
+			unset($me["id"] );
 			$db->merge( $me );
 		}
 		// - if logged in to the api assume it has the latest oauth info
-		if( $api && $api->login() ) {
+		if( $api && $api->login() && array_key_exists('oauth', $_SESSION) ) {
 			//
-			$db->set('oauth', $api->oauth->creds() );
+			//$db->set('oauth', $api->oauth->creds() );
+			$db->set('oauth', $_SESSION['oauth'] );
 
 		} else if( !is_null( $db->get('oauth') ) ) {
 			// check for credentials that have expired?
 			// use the creds from the db
-			$api->oauth->creds( $db->get('oauth') );
-			// re-initiate service (to login to the api)
-			$api->init();
+			$oauth = (array) $db->get('oauth');
+			if( is_array($oauth) && array_key_exists($api->name, $oauth) ){
+				$api->oauth->creds( $oauth[ $api->name ] );
+				// re-initiate service (to login to the api)
+				$api->init();
+			}
 		}
+
 		// - record the time
 		$db->set("updated", timestamp() );
-
+		$db->extend("accounts", array( "facebook" => $this->getAPIdetails( $api->me() ) ) );
 		// STEP 4: SAVE back to the db
 		// - update the user model
-		$method = ( !$store) ? "create" : "update";
+		$method = ( !isset($id) ) ? "create" : "update";
 		$db->{$method}();
 
 		// - set the user cookie (expires in one day)
-		setcookie("user", $id, time()+86400);
+		//setcookie("user", $db->get("id"), time()+86400); // disabling temporarily (not working in 5.3)
 		// - set the session
 		$_SESSION['user'] = $db->getAll();
 
@@ -99,6 +116,16 @@ class KISS_Auth extends Controller {
 
 	}
 
+	function getAPIdetails( $data ){
+		$details = array();
+
+		$fields = array("id", "username", "email", "name");
+		foreach( $fields as $field ){
+			if( array_key_exists( $field, $data ) ) $details[$field] = $data[$field];
+		}
+
+		return $details;
+	}
 
 }
 }

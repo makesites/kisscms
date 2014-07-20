4f7ea501c5997b97d97c8a71b6000acfd827f4a7
Makis Tracend <makis.tracend@gmail.com>
2014-07-19 23:42:35 -0700

    :zap: first-time run page model

--- app/models/Page.php
+++ app/models/Page.php
@@ -77,14 +77,14 @@ class Page extends Model {
 		$results = $dbh->prepare($sql);
 		$results->execute();
 		$pages = $results->fetch(PDO::FETCH_ASSOC);
-		$GLOBALS['db_schema']['pages'] = array_keys( $pages );
+		$GLOBALS['db_schema']['pages'] = ( is_array($pages) ) ?  array_keys( $pages ) : array();
 	}
 
 	// add the column if necessary
 	if( !in_array($key, $GLOBALS['db_schema']['pages']) ){
 		$sql = "ALTER TABLE pages ADD COLUMN ". $key;
 		$results = $dbh->prepare($sql);
-		$results->execute();
+		if( $results ) $results->execute(); // there's a case where the column may already exist, in which case this will be false...
 		array_push( $GLOBALS['db_schema']['pages'], $key );
 	}
 

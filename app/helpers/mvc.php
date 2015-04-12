82448975515fec21eba4777facf056045830c351
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 07:12:21 -0700

    :star: controller event setter and trigger

--- app/helpers/mvc.php
+++ app/helpers/mvc.php
@@ -463,6 +463,34 @@ class Controller extends KISS_Controller {
 
 	}
 
+	/* Events */
+	public static function on( $event=false, $class=false ){
+		// prerequisites
+		if( !$event || !$class ) return;
+		//
+		if( !array_key_exists('events', $GLOBALS) ) $GLOBALS['events'] = array();
+		if( !array_key_exists($event, $GLOBALS['events']) ) $GLOBALS['events'][$event] = array();
+		//
+		$GLOBALS['events'][$event][] = $class;
+	}
+
+	public static function trigger( $event=false, &$data=false ){
+		// prerequisites
+		if( !$event || !$data ) return;
+		if( !array_key_exists('events', $GLOBALS) ) return;
+		if( !array_key_exists($event, $GLOBALS['events']) ) return;
+		//
+		$classes = $GLOBALS['events'][$event];
+
+		// convention: remove prefix- from event to reveal action
+		$action = ( strpos($event, '-') ) ? substr( $event, strpos($event, '-')+1 ): $event;
+
+		foreach( $classes as $class ){
+			$class::$action( $data );
+		}
+
+	}
+
 	/**
 	 *  CORS-compliant method.  It will allow any GET, POST, PUT or DELETE requests from any origin.
 	 *

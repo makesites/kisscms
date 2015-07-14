a8cbe61d418ae86e978a123540785fa28de16541
Makis Tracend <makis.tracend@gmail.com>
2015-07-14 03:43:09 -0700

    :zzz: ws

--- app/controllers/tag.php
+++ app/controllers/tag.php
@@ -24,7 +24,7 @@ class Tag extends Controller {
 		// get the page details stored in the database
 		$this->requestAllPages();
 
-		// define the rendereing template
+		// define the rendering template
 		if( !$this->data['template'] ) $this->data['template'] = LISTINGS_TEMPLATE;
 
 		// display the page

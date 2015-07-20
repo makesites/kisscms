26984f73965de75d621a4d6316f43e0652ea72ea
Makis Tracend <makis.tracend@gmail.com>
2015-07-20 11:25:03 -0700

    :zap: registering tags column

--- app/bin/pages.php
+++ app/bin/pages.php
@@ -9,6 +9,7 @@ if( class_exists('Page') && method_exists(new Page(),'register')){
 	Page::register("1", "title", "Welcome");
 	Page::register("1", "content", "Content Reset... If this was by mistake please restore your database.");
 	Page::register("1", "path", "");
+	Page::register("1", "tags", "");
 	Page::register("1", "template", $GLOBALS['config']['main']['default_template']);
 	Page::register("1", "created", time('now') );
 	Page::register("1", "updated", time('now') );

af631634ac5f1e868afba8f0d737fdcb3cd85115
Makis Tracend <makis.tracend@gmail.com>
2016-01-29 15:29:57 -0800

    #136 removing ChromeFrame

--- app/views/main/foot.php
+++ app/views/main/foot.php
@@ -1,9 +1,3 @@
-
-<!--[if lt IE 7 ]>
-<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-<![endif]-->
-
 <script type="text/javascript" data-type="client">
 	Object.extend(KISSCMS, {"require":{"callback": function(){ if(typeof init != "undefined") init(); } }});
 </script>
